267297f0f651740ef53ff8edb048d2a826b721d2
mgmgmg123123 <55453461+mgmgmg123123@users.noreply.github.com>
2023-10-16 03:08:15 +0900

    feat: ハンバーガーJS、背景ロック、リサイズ時の処理を追加

--- src/assets/js/hamburgerMenu.ts
+++ src/assets/js/hamburgerMenu.ts
@@ -2,12 +2,14 @@ class HamburgerMenu {
   menu: HTMLHtmlElement;
   button: HTMLHtmlElement;
   menuLinks: NodeListOf<HTMLAnchorElement>;
+  isSupport: boolean;
 
   constructor() {
     this.menu = document.querySelector(".js-hamburger-menu")!;
     this.button = document.querySelector(".js-hamburger")!;
     this.menuLinks = this.menu.querySelectorAll("a")!;
-    console.log("constructor!");
+    this.isSupport = CSS.supports("overscroll-behavior", "none");
+
     this.init();
   }
   init() {
@@ -16,6 +18,15 @@ class HamburgerMenu {
     });
     this.button.addEventListener("click", () => {
       this.toggleMenu();
+    });
+
+    // ハンバーガー開けっぱなしで画面サイズをPCサイズへ変えた時に閉じる
+    const mdSize = 768;
+    const mediaQueryList = window.matchMedia(`(min-width: ${mdSize}px)`);
+    mediaQueryList.addEventListener("change", (event) => {
+      if (event.matches) {
+        this.closeMenu();
+      }
     });
   }
   menuLinkClicked() {
@@ -35,11 +46,19 @@ class HamburgerMenu {
     this.menu.classList.add("_isActive");
     this.button.setAttribute("aria-label", "メニューを開く");
     this.button.setAttribute("aria-expanded", "true");
+    this.bodyLock();
   }
   closeMenu() {
     this.menu.classList.remove("_isActive");
     this.button.setAttribute("aria-label", "メニューを表示する");
     this.button.setAttribute("aria-expanded", "false");
+    this.bodyUnlock();
+  }
+  bodyLock() {
+    document.body.classList.add("_isScrollLock");
+  }
+  bodyUnlock() {
+    document.body.classList.remove("_isScrollLock");
   }
 }
 export const hamburgerMenu = () => {
