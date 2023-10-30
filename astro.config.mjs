3eb5d79629c3303d1f028d9064068be737f47954
mgmgmg123123 <55453461+mgmgmg123123@users.noreply.github.com>
2023-10-30 10:23:27 +0900

    feat: GitHubActions設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mgmgmg123123.github.io",
+  base: "/site-training-2",
+});
