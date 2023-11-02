e51262d28f816497365ee452f0dab003a418ce64
mgmgmg123123 <55453461+mgmgmg123123@users.noreply.github.com>
2023-11-03 00:21:20 +0900

    feat: パス整理

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mgmgmg123123.github.io",
-  base: "/site-training-2",
+  base: "site-training-2",
 });
---
+++ src/utils/getPath.ts
@@ -0,0 +1,6 @@
+const base = "site-training-2";
+export const getPath = {
+  top: `/${base}`,
+  message: `/${base}/message`,
+  company: `/${base}/company`,
+};
